5da655e715e50c66990bf2da8a03c1738a570869
Tim Burks <timburks@google.com>
2018-04-06 23:00:27 -0700

    demo-3: add libshapes (text display is currently broken)

---
+++ macosx/demo-3/src/eglstate.h
@@ -0,0 +1,13 @@
+typedef struct {
+	// Screen dimentions
+	uint32_t screen_width;
+	uint32_t screen_height;
+	// Window dimentions
+	int32_t window_x;
+	int32_t window_y;
+	uint32_t window_width;
+	uint32_t window_height;
+
+} STATE_T;
+
+extern void oglinit(STATE_T *);
