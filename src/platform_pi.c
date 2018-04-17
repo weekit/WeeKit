da954a3311f08c1204d9ed65511e6a6722381169
Tim Burks <timburks@google.com>
2018-04-16 20:51:08 -0800

    simplify platform_pi.c

--- src/platform_pi.c
+++ src/platform_pi.c
@@ -30,14 +30,9 @@ typedef struct {
 
 	// EGL data
 	EGLDisplay display;
-
 	EGLSurface surface;
 	EGLContext context;
 } STATE_T;
-
-extern void oglinit(STATE_T *);
-extern void dispmanMoveWindow(STATE_T *, int, int);
-extern void dispmanChangeWindowOpacity(STATE_T *, unsigned int);
 
 static STATE_T _state, *state = &_state;	// global graphics state
 static const int MAXFONTPATH = 500;
@@ -46,23 +41,7 @@ static int init_y = 0;
 static unsigned int init_w = 0;
 static unsigned int init_h = 0;
 
-// dumpscreen writes the raster
-void dumpscreen(int w, int h, FILE * fp) {
-	void *ScreenBuffer = malloc(w * h * 4);
-	vgReadPixels(ScreenBuffer, (w * 4), VG_sABGR_8888, 0, 0, w, h);
-	fwrite(ScreenBuffer, 1, w * h * 4, fp);
-	free(ScreenBuffer);
-}
-
-// initWindowSize requests a specific window size & position, if not called
-// then init() will open a full screen window.
-// Done this way to preserve the original init() behaviour.
-void egl_initWindowSize(int x, int y, unsigned int w, unsigned int h) {
-	init_x = x;
-	init_y = y;
-	init_w = w;
-	init_h = h;
-}
+void oglinit(STATE_T * state);
 
 // init sets the system to its initial state
 void egl_init(int *w, int *h) {
@@ -86,46 +65,9 @@ void egl_finish() {
 	eglTerminate(state->display);
 }
 
-// End checks for errors, and renders to the display
-void End() {
-	assert(vgGetError() == VG_NO_ERROR);
-	eglSwapBuffers(state->display, state->surface);
-	assert(eglGetError() == EGL_SUCCESS);
-}
-
-// SaveEnd dumps the raster before rendering to the display 
-void SaveEnd(const char *filename) {
-	FILE *fp;
-	assert(vgGetError() == VG_NO_ERROR);
-	if (strlen(filename) == 0) {
-		dumpscreen(state->screen_width, state->screen_height, stdout);
-	} else {
-		fp = fopen(filename, "wb");
-		if (fp != NULL) {
-			dumpscreen(state->screen_width, state->screen_height, fp);
-			fclose(fp);
-		}
-	}
-	eglSwapBuffers(state->display, state->surface);
-	assert(eglGetError() == EGL_SUCCESS);
-}
-
-// Backgroud clears the screen to a solid background color
-
-// WindowOpacity sets the  window opacity
-void WindowOpacity(unsigned int a) {
-	dispmanChangeWindowOpacity(state, a);
-}
-
-// WindowPosition moves the window to given position
-void WindowPosition(int x, int y) {
-	dispmanMoveWindow(state, x, y);
-}
-
 
 // WeeKit handler functions
 typedef void (*WKDrawHandler)(int, int);
-
 
 // Handler pointers
 WKDrawHandler wkDrawHandler;
@@ -140,69 +82,6 @@ int WKMain(WKDrawHandler handler) {
   sleep(5);
   egl_finish();
   return 0;
-}
-
-// setWindowParams sets the window's position, adjusting if need be to
-// prevent it from going fully off screen. Also sets the dispman rects
-// for displaying.
-static void setWindowParams(STATE_T * state, int x, int y, VC_RECT_T * src_rect, VC_RECT_T * dst_rect) {
-	uint32_t dx, dy, w, h, sx, sy;
-
-	// Set source & destination rectangles so that the image is
-	// clipped if it goes off screen (else dispman won't show it properly)
-	if (x < (1 - (int)state->window_width)) {	   // Too far off left
-		x = 1 - (int)state->window_width;
-		dx = 0;
-		sx = state->window_width - 1;
-		w = 1;
-	} else if (x < 0) {				   // Part of left is off
-		dx = 0;
-		sx = -x;
-		w = state->window_width - sx;
-	} else if (x < (state->screen_width - state->window_width)) {	// On
-		dx = x;
-		sx = 0;
-		w = state->window_width;
-	} else if (x < state->screen_width) {		   // Part of right is off
-		dx = x;
-		sx = 0;
-		w = state->screen_width - x;
-	} else {					   // Too far off right
-		x = state->screen_width - 1;
-		dx = state->screen_width - 1;
-		sx = 0;
-		w = 1;
-	}
-
-	if (y < (1 - (int)state->window_height)) {	   // Too far off top
-		y = 1 - (int)state->window_height;
-		dy = 0;
-		sy = state->window_height - 1;
-		h = 1;
-	} else if (y < 0) {				   // Part of top is off
-		dy = 0;
-		sy = -y;
-		h = state->window_height - sy;
-	} else if (y < (state->screen_height - state->window_height)) {	// On
-		dy = y;
-		sy = 0;
-		h = state->window_height;
-	} else if (y < state->screen_height) {		   // Part of bottom is off
-		dy = y;
-		sy = 0;
-		h = state->screen_height - y;
-	} else {					   // Wholly off bottom
-		y = state->screen_height - 1;
-		dy = state->screen_height - 1;
-		sy = 0;
-		h = 1;
-	}
-
-	state->window_x = x;
-	state->window_y = y;
-
-	vc_dispmanx_rect_set(dst_rect, dx, dy, w, h);
-	vc_dispmanx_rect_set(src_rect, sx << 16, sy << 16, w << 16, h << 16);
 }
 
 // oglinit sets the display, OpenVGL context and screen information
@@ -264,10 +143,6 @@ void oglinit(STATE_T * state) {
 	if ((state->window_height == 0) || (state->window_height > state->screen_height))
 		state->window_height = state->screen_height;
 
-	// adjust position so that at least one pixel is on screen and
-	// set up the dispman rects
-	setWindowParams(state, state->window_x, state->window_y, &src_rect, &dst_rect);
-
 	dispman_display = vc_dispmanx_display_open(0 /* LCD */ );
 	dispman_update = vc_dispmanx_update_start(0);
 
@@ -293,30 +168,3 @@ void oglinit(STATE_T * state) {
 	assert(EGL_FALSE != result);
 }
 
-// dispmanMoveWindow repositions the openVG window to given coords
-// -ve coords are allowed upto (1-width,1-height),
-// max (screen_width-1,screen_height-1). i.e. at least one pixel must be
-// on the screen.
-void dispmanMoveWindow(STATE_T * state, int x, int y) {
-	VC_RECT_T src_rect, dst_rect;
-	DISPMANX_UPDATE_HANDLE_T dispman_update;
-
-	setWindowParams(state, x, y, &src_rect, &dst_rect);
-	dispman_update = vc_dispmanx_update_start(0);
-	vc_dispmanx_element_change_attributes(dispman_update, state->element, 0, 0, 0, &dst_rect, &src_rect, 0, DISPMANX_NO_ROTATE);
-	vc_dispmanx_update_submit_sync(dispman_update);
-}
-
-// dispmanChangeWindowOpacity changes the window's opacity
-// 0 = transparent, 255 = opaque
-void dispmanChangeWindowOpacity(STATE_T * state, uint32_t alpha) {
-	DISPMANX_UPDATE_HANDLE_T dispman_update;
-
-	if (alpha > 255)
-		alpha = 235;
-
-	dispman_update = vc_dispmanx_update_start(0);
-	// The 1<<1 below means update the alpha value
-	vc_dispmanx_element_change_attributes(dispman_update, state->element, 1 << 1, 0, alpha, 0, 0, 0, DISPMANX_NO_ROTATE);
-	vc_dispmanx_update_submit_sync(dispman_update);
-}
