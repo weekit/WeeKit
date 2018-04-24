8f16441366019f6a6cf9d30f8ee9fedf5e9af8e3
Tim Burks <timburks@google.com>
2018-04-23 23:03:30 -0700

    minor restructuring

--- rust/src/weekit/mod.rs
+++ rust/src/weekit/mod.rs
@@ -12,14 +12,23 @@ extern crate libc;
 use std::sync::Arc;
 use std::sync::Mutex;
 
-#[link(name = "wee")]
-extern "C" {
-    fn WKMain(f: extern "C" fn(u32, u32) -> (), e: extern "C" fn(u16, u16, i32) -> ()) -> i64;
-}
-
 pub trait Application {
     fn draw(&mut self, width: u32, height: u32) -> ();
     fn event(&mut self, event: &input::Event) -> ();
+}
+
+// main should be called from client applications to run the main event loop.
+pub fn main<T: Application + 'static>(application: T) -> i64 {
+    unsafe {
+        APPLICATION = Some(Arc::new(Mutex::new(application)));
+        TOUCH_PAD = Some(input::TouchPad::new());
+        return WKMain(draw_handler_wrapper, event_handler_wrapper);
+    }
+}
+
+#[link(name = "wee")]
+extern "C" {
+    fn WKMain(f: extern "C" fn(u32, u32) -> (), e: extern "C" fn(u16, u16, i32) -> ()) -> i64;
 }
 
 static mut APPLICATION: Option<Arc<Mutex<Application>>> = None;
@@ -47,14 +56,5 @@ extern "C" fn event_handler_wrapper(t: u16, c: u16, v: i32) -> () {
             Some(ref mut touchpad) => touchpad.handle(t, c, v, APPLICATION.clone()),
             None => {}
         }
-    }
-}
-
-// main should be called from client applications to run the main event loop.
-pub fn main<T: Application + 'static>(application: T) -> i64 {
-    unsafe {
-        APPLICATION = Some(Arc::new(Mutex::new(application)));
-        TOUCH_PAD = Some(input::TouchPad::new());
-        return WKMain(draw_handler_wrapper, event_handler_wrapper);
     }
 }
