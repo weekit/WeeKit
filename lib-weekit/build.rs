6b9756ee493cd582940bada3a6d6402c0f80af17
Tim Burks <timburks@google.com>
2018-04-28 18:06:43 -0700

    restructuring

--- lib-weekit/build.rs
+++ lib-weekit/build.rs
@@ -3,11 +3,11 @@ use std::env;
 fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("apple") {
-        println!("cargo:rustc-link-search=native=../Demo.app/Contents/dylibs");
+        println!("cargo:rustc-link-search=native=../platform/lib");
         println!("cargo:rustc-link-lib=platform");
         println!("cargo:rustc-link-lib=AmanithVG.4");
     } else {
-        println!("cargo:rustc-link-search=native=../platform");
+        println!("cargo:rustc-link-search=native=../platform/lib");
         println!("cargo:rustc-link-search=native=/opt/vc/lib");
         println!("cargo:rustc-link-lib=platform");
         println!("cargo:rustc-link-lib=brcmEGL");
