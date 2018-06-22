e09f8b255a3bfbf8647bb8b4105b98325128a4d2
Tim Burks <timburks@google.com>
2018-06-21 21:44:46 -0800

    Reading inputs on Raspberry Pi.

---
+++ experimental/reader/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::slice;
+use std::io::Read;
+use libc::timeval;
+use std::mem;
+use std::thread;
+
+extern crate libc;
+
+#[repr(C, packed)]
+struct InputEvent {
+        time: timeval,
+        kind: u16,
+        code: u16,
+        value: i32
+}
+
+fn main() {
+    println!("Hello, world!");
+    handle_inputs("/dev/input/touchscreen");
+    handle_inputs("/dev/input/keyboard");
+    loop {}
+}
+
+fn handle_inputs(filename: &'static str) {
+thread::spawn(move || {
+    let mut f = File::open(filename).expect(&("unable to open ".to_owned()+filename));
+
+    let mut input_event : InputEvent = unsafe { mem::zeroed() };
+    let input_event_size = mem::size_of::<InputEvent>();
+    loop {
+        unsafe {
+	    let input_event_slice = slice::from_raw_parts_mut(
+	    &mut input_event as *mut _ as *mut u8, input_event_size);
+            f.read_exact(input_event_slice).unwrap();
+            println!("{} {} {}", input_event.kind, input_event.code, input_event.value);
+        }
+    }
+});
+}
