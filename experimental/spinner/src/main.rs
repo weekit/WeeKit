a43b623d715ff7b471e26a4cf96e7180cc3f21da
Tim Burks <timburks@google.com>
2018-06-24 17:09:09 -0700

    Give the spinner sample some work to do in each thread.

--- experimental/spinner/src/main.rs
+++ experimental/spinner/src/main.rs
@@ -1,23 +1,29 @@
-use std::thread;
 use std::env;
+use std::thread;
 
-fn spin() {
-    thread::spawn(move || {
-	loop {}
-    });
+fn spin(i: i32) {
+    loop {
+        let mut sum: f64 = 0.0;
+        for i in 1..10000000 {
+            sum += (i as f64).sqrt();
+        }
+        println!("{}: {}", i, sum);
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut threads = 1;
     if args.len() > 1 {
-	threads = args[1].parse().unwrap();
+        threads = args[1].parse().unwrap();
     }
     println!("Hello! Running with {} threads.", threads);
-    for _ in 1..threads {
-        spin();
+    for i in 1..threads {
+        thread::spawn(move || {
+            spin(i);
+        });
         println!("launching");
     }
     println!("launching");
-    loop {}
+    spin(0);
 }
