0e995a34a137bc0c8ac3e1fb6874f92d59eefdee
Tim Burks <timburks@google.com>
2018-07-06 20:33:02 -0800

    Moved all pi build steps into cargo and build.rs.

--- rust/weekit/build.rs
+++ rust/weekit/build.rs
@@ -33,12 +33,21 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=OpenGL");
         println!("cargo:rustc-link-lib=framework=QuartzCore");
     } else {
-        println!("cargo:rustc-link-search=native=../../lib");
+        let out_dir = env::var("OUT_DIR").unwrap();
+	println!("OUT_DIR = {}", out_dir);
+        Command::new("make")
+            .arg(&"-f")
+            .arg(&"tools/Makefile.pi")
+            .status()
+            .unwrap();
+        println!("cargo:rustc-link-search=native={}", out_dir);
         println!("cargo:rustc-link-search=native=/opt/vc/lib");
         println!("cargo:rustc-link-lib=platform");
-        println!("cargo:rustc-link-lib=brcmEGL");
-        println!("cargo:rustc-link-lib=brcmGLESv2");
+        println!("cargo:rustc-link-lib=jpeg");
         println!("cargo:rustc-link-lib=brcmOpenVG");
+        println!("cargo:rustc-link-lib=brcmEGL");
         println!("cargo:rustc-link-lib=bcm_host");
+        println!("cargo:rustc-link-lib=vcos");
+        println!("cargo:rustc-link-lib=brcmGLESv2");
     }
 }
