4c2066587d2f55124dc707540e510811d66da25d
Tim Burks <timburks@google.com>
2018-07-07 13:15:53 -0700

    Flatten project directory structure.

--- build.rs
+++ build.rs
@@ -27,14 +27,16 @@ fn main() {
         println!("cargo:rustc-link-lib=platform");
         println!("cargo:rustc-link-search=native=/usr/local/lib");
         println!("cargo:rustc-link-lib=jpeg");
-        println!("cargo:rustc-link-search=native=../../third-party/amanithvg-sdk/lib/macosx/ub/gle/standalone");
+        println!(
+            "cargo:rustc-link-search=native=third-party/amanithvg-sdk/lib/macosx/ub/gle/standalone"
+        );
         println!("cargo:rustc-link-lib=AmanithVG.4");
         println!("cargo:rustc-link-lib=framework=Cocoa");
         println!("cargo:rustc-link-lib=framework=OpenGL");
         println!("cargo:rustc-link-lib=framework=QuartzCore");
     } else {
         let out_dir = env::var("OUT_DIR").unwrap();
-	println!("OUT_DIR = {}", out_dir);
+        println!("OUT_DIR = {}", out_dir);
         Command::new("make")
             .arg(&"-f")
             .arg(&"tools/Makefile.pi")
